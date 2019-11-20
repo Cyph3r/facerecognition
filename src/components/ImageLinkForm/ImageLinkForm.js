330811e0a186a5c73765212ba58825d393907d5a
Derek <derekj77@gmail.com>
2019-11-20 10:53:47 -0600

    CSS

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./ImageLinkForm.css";
 
 const ImageLinkForm = () => {
     return (
@@ -8,11 +9,13 @@ const ImageLinkForm = () => {
                     "This Magic Brain will detect the location of faces in your pictures. Give it a try!"
                 }
             </p>
-            <div>
-                <input className="f4 pa2 w-70 center" type="text" />
-                <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
-                    Detect
-                </button>
+            <div className="center">
+                <div className="form center pa3 br3 shadow-5">
+                    <input className="f4 pa2 w-70 center" type="text" />
+                    <button className="w-30 grow f4 link ph3 pv2 dib white">
+                        Detect
+                    </button>
+                </div>
             </div>
         </div>
     );
